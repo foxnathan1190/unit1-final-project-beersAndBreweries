318e060955a1821d707fc1f1964e80ad42ae68c9
foxnathan1190 <foxnathan1190@gmail.com>
2025-07-23 20:19:48 -0500

    Made final touches.

--- beers-and-breweries/src/components/About.jsx
+++ beers-and-breweries/src/components/About.jsx
@@ -4,7 +4,7 @@ import "./About.css";
 import NavigationMenu from "../common/NavigationMenu";
 import peopleAtBrewery from "../images/peopleAtBrewery.jpg";
 
-const About = ({isLoggedIn }) => {
+const About = ({ isLoggedIn }) => {
 
     return (
         <div >
@@ -14,7 +14,19 @@ const About = ({isLoggedIn }) => {
                 <h1 id="aboutHeader">About Us</h1>
                 <img src={peopleAtBrewery} alt="People at a Brewery" id="aboutImage" />
                 <p>
-                    We here at Drink Local have always wanted to be able to travel and simply drink local. We are a small midwestern operation from St. Louis Missouri and want you to be able to experience breweries of every kind no matter where you are going.<br /><br /> With our search feature you can enter the city you are in or travleing to and it will provide you with a list of all breweries in that city, along with their website. We want you to be able to experience local breweries and not be worried about how to find them. <br /><br />Another great use of our search is to find out where that brewery is that makes the beer you found at the local grocery store or maybe that beer you tried at dinner once. You can enter the brewer from the label and get more imformation on them.<br /><br />We also want you to be safe and stick around with out website, so we ask that you make a profile and introduce yourself to use. We want you to be of age and also let us know your favorite brewery. Hopefully, in the future you'll be able to save breweries to your profile that you've been to our want to try. <br /><br />Until then our search awaits that next trip where you choose to Drink Local!
+                    We at Drink Local have always wanted to be able to travel and simply drink local. We are a small midwestern operation from St. Louis Missouri and want you to be able to experience breweries of every kind, no matter where you are going.
+                </p>
+                <p>
+                    With our patented search feature, you can enter the city you are in or travleing to and it will provide you with a list of all breweries in that city, along with their website. We want you to be able to experience local breweries and not be worried about how to find them.
+                </p>
+                <p>
+                    Another great use of our search feature is to find where the brewery is that makes the beer you found at the local grocery store or maybe the beer you tried at dinner. You can enter the brewer from the label and find out more imformation.
+                </p>
+                <p>
+                    We also want you to stick around, so we ask that you make a profile and introduce yourself. We want you to be of age and also let us know your favorite brewery.
+                </p>
+                <p>
+                    Your next search on Drink Local awaits!
                 </p>
             </section>
             <Footer />
--- beers-and-breweries/src/components/Main.jsx
+++ beers-and-breweries/src/components/Main.jsx
@@ -14,14 +14,17 @@ const Main = ({ isLoggedIn }) => {
             <NavigationMenu isLoggedIn={isLoggedIn} />
             <div className="layout">
                 <h1 className="mainH1">Welcome to Drink Local!</h1>
-                <p >
-                    Drink Local, the site you use to find that local brewery you've been dreaming of. That Brewery with great beer and a great vibe. Use our patented search feature and extensive database of breweries to find the breweries in your hometown or the place to hang out while traveling.<br /><br /> Whatever your reason for being here, we're glad you are. Take a look around and we hope you keep coming back again and again.
+                <p>
+                    Drink Local, the site you use to find the local brewery you've been dreaming of. The brewery with great beer and a great vibe. Use our patented search feature and extensive database to find breweries in your hometown or the place to be while traveling.
+                </p>
+                <p>
+                    Whatever your reason for being here, we're glad you are. Take a look around and we hope you keep coming back again and again.
                 </p><br />
                 <div>
-                    <img src={urbanChestnut} alt="Urban Chestnut Brewery" className="hover-over-image"/>
-                    <img src={greatDivide} alt="Great Divide Brewery" className="hover-over-image"/><br />
-                    <img src={austinBeerworks} alt="Austin Beerworks Brewery" className="hover-over-image"/>
-                    <img src={rooftopBrewCo} alt="Rooftop Brewing Company" className="hover-over-image"/>
+                    <img src={urbanChestnut} alt="Urban Chestnut Brewery" className="hover-over-image" />
+                    <img src={greatDivide} alt="Great Divide Brewery" className="hover-over-image" /><br />
+                    <img src={austinBeerworks} alt="Austin Beerworks Brewery" className="hover-over-image" />
+                    <img src={rooftopBrewCo} alt="Rooftop Brewing Company" className="hover-over-image" />
                 </div>
             </div>
             <Footer />
